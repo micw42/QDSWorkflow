ca72751dfa3e9534335e0d36f01b75bc8b656d84
micw42 <weimichelles@gmail.com>
2023-02-26 17:29:32 -0700

    Updated LC ann

--- data-raw/lc_ann.R
+++ data-raw/lc_ann.R
@@ -1,3 +1,9 @@
-lc_ann= readRDS("lung_cancer_ann.txt")
+lc_ann = lc_ann %>% 
+  mutate(Development_Stage=case_when(Sample_Origin=="nLung" ~"normal lung",
+                                     Sample_Origin=="tLung" ~"lung tumor (early)",
+                                     Sample_Origin=="tL/B" ~"lung tumor (late)",
+                                     Sample_Origin=="mLN" ~"lymph node (metastasis)",
+                                     Sample_Origin=="PE" ~"pleural effusion (metastasis)",
+                                     Sample_Origin=="mBrain" ~"brain (metastasis)"))
 setwd("QDSWorkflow/")
-usethis::use_data(lc_ann)
+usethis::use_data(lc_ann, overwrite = T)
